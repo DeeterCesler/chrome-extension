d4d93786d0ab7e60391b6c38e197819ef1322bff
Deeter <deeter.cesler@gmail.com>
2019-03-24 20:33:07 -0600

    cleaned up old methods of insertion

--- contentScript.js
+++ contentScript.js
@@ -1,30 +1,3 @@
-// var elements = document.getElementsByTagName("*");
-
-// for (let i = 0; i < elements.length; i++) {
-//     let element = elements[i];
-
-//     for(var j=0; j < element.childNodes.length; j++){
-//         var node = element.childNodes[j];
-
-//         if(node.nodeType === 3){
-//             var text = node.nodeValue;
-//             var grade = "doggy"
-//             let replacedText = text.replace("the", <span color="blue">doggy</span>);
-            
-//             if(replacedText !== text) {
-//                 // node.appendChild(para);
-//                 element.replaceChild(document.createTextNode(replacedText), node);
-//             }
-//         }
-//     }
-// }
-
-// var grade = "B+"
-
-// document.body.innerHTML = document.body.innerHTML.replace(new RegExp("\\b" + "Boeing" + "\\b", "g"),
-//                                                           `<span style='background-color: rgba(55,55,55, .3); border-radius: 3px; padding: 2px;'>$&</span>`); 
-
-
 var matchText = function(node, regex, callback, excludeElements) { 
 
     excludeElements || (excludeElements = ['script', 'style', 'iframe', 'canvas']);
